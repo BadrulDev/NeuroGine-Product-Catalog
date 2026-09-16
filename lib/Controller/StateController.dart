168a9cf6d95ba2de0c27169d097509c522d2ba22
Badrul-Muhymin <aminmuhymin07@gmail.com>
2026-09-16 10:04:00 +0800

    -added http for fetching data via cdn provided -created data controller to do all the data fetching(all, by id, search) -create object for both provider and data controller on the homepage -introduce limit and skips on provider

--- lib/Controller/StateController.dart
+++ lib/Controller/StateController.dart
@@ -2,11 +2,24 @@ import 'package:flutter/foundation.dart';
 
 class StateController with ChangeNotifier {
   bool _isLoading = false;
+  int _limit = 0;
+  int _skip = 0;
 
   bool get isLoggedIn => _isLoading;
+  int get limit => _limit;
 
   void updateLoadingStatus(bool status) {
     _isLoading = status;
+    notifyListeners();
+  }
+
+  void updateLimit(int limit) {
+    _limit = limit;
+    notifyListeners();
+  }
+
+  void updateSkip(int skip) {
+    _skip = skip;
     notifyListeners();
   }
 }
