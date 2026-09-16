ee4bdd3446de2a72129af98c7495df053ef14943
Badrul-Muhymin <aminmuhymin07@gmail.com>
2026-09-16 10:09:54 +0800

    -added initial state(for setting limit and skips on provider, to retrieve data for initial) -added dispose(clearing all data, limit and skips) -soon to be added

--- lib/Interface/Homepage.dart
+++ lib/Interface/Homepage.dart
@@ -14,6 +14,16 @@ class _HomepageState extends State<Homepage> {
   final StateController _stateController = StateController();//for data from provider
   final DataController _dataController = DataController();//for data from CDN provided
 
+  @override
+  void initState() {
+    super.initState();
+
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
